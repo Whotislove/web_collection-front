1339a21f1fbb5ba68dcab2be741c0fa15553c539
Whotislove <vlad.lobunets@mail.ru>
2022-10-26 22:29:07 +0300

    ad

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   // http://localhost:1111
+  // adaw
 });
 
 instance.interceptors.request.use((config) => {
