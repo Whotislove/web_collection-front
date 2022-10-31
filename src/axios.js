340a88af2bedf240f3551432dc342738082a980c
Whotislove <vlad.lobunets@mail.ru>
2022-10-31 17:16:40 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:1111',
+  baseURL: process.env.REACT_APP_API_URL,
   // http://localhost:1111
   // process.env.REACT_APP_API_URL
   // adaw
